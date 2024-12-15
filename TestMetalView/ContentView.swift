2d0e671f31832c7f04ac590116d967c3654fd281
Boris Bugor <bugor.bn@icloud.com>
2024-12-15 19:48:05 +0200

    Initial Commit

---
+++ TestMetalView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestMetalView
+//
+//  Created by Boris Bugor on 15/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestMetalView/TestMetalViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMetalViewApp.swift
+//  TestMetalView
+//
+//  Created by Boris Bugor on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestMetalViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
